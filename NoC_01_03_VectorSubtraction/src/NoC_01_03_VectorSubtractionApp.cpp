83a05e9fae7c6ec94d9ef9891fe2e2553992697c
David <davidptracy@gmail.com>
2014-06-10 17:09:33 -0400

    fixed example 3

--- NoC_01_03_VectorSubtraction/src/NoC_01_03_VectorSubtractionApp.cpp
+++ NoC_01_03_VectorSubtraction/src/NoC_01_03_VectorSubtractionApp.cpp
@@ -13,9 +13,9 @@ using namespace std;
 
 class NoC_01_03_VectorSubtractionApp : public AppNative {
   public:
-	void setup();
-    void prepareSettings(Settings *settings);
-	void mouseMove( MouseEvent event);
+	void prepareSettings(Settings *settings);
+    void setup();
+    void mouseMove( MouseEvent event);
 	void update();
 	void draw();
     
@@ -23,16 +23,16 @@ class NoC_01_03_VectorSubtractionApp : public AppNative {
     Vec2f mMouseLoc;
 };
 
-void NoC_01_03_VectorSubtractionApp::setup()
-{
-    //this is where mMouseLoc is initialized
-    mMouseLoc = getWindowCenter();
-}
-
 //the * symbol is a dereference of a pointer and refers to the value stored at the location 'settings'
 void NoC_01_03_VectorSubtractionApp::prepareSettings
 (Settings *settings){
     settings->setWindowSize(500, 500);
+}
+
+void NoC_01_03_VectorSubtractionApp::setup()
+{
+    //this is where mMouseLoc is initialized
+    mMouseLoc = getWindowCenter();
 }
 
 //this function runs only if the mouse is moving
@@ -43,8 +43,6 @@ void NoC_01_03_VectorSubtractionApp::mouseMove(MouseEvent event){
 
 void NoC_01_03_VectorSubtractionApp::update()
 {
-    //vector subtraction
-    mMouseLoc = mMouseLoc - getWindowCenter();
 }
 
 void NoC_01_03_VectorSubtractionApp::draw()
@@ -52,10 +50,17 @@ void NoC_01_03_VectorSubtractionApp::draw()
 	// clear out the window with black
 	gl::clear( Color( 0, 0, 0 ) );
     
+    //the vector mouse is being created from the mouseMove function so that when the mouse isn't moving, the position drawn on screen is maintained
+    Vec2f mouse = mMouseLoc;
+    Vec2f center = getWindowCenter();
+    
+    mouse -= center;
+    
     gl::pushMatrices();
     gl::translate(getWindowCenter());
+    gl::lineWidth(2.0);
     //draws a line from the center of the screen to mMouseLoc
-    gl::drawLine(Vec2f::zero(), mMouseLoc);
+    gl::drawLine(Vec2f::zero(), mouse);
     gl::popMatrices();
 }
 
